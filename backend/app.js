13ba9a5b4dae64f1c519ac7647e09f9c200dddd5
eric kufta <eric.kufta@gmail.com>
2024-02-27 11:24:55 -0800

    adding signup and mongo stuff

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const underscore = require('underscore');
 const mongoose = require('mongoose');
 const Countries = require('./models/countries');
 const Customers = require('./models/customers');
@@ -11,11 +10,9 @@ const Customers = require('./models/customers');
 const app = express();
 
 // Connect to MongoDB
-
-//Fill in Password!!!
-const dbURI = 'mongodb+srv://erickufta:<password>.@cluster0.a3a8ugn.mongodb.net/NomadTaxProMain?retryWrites=true&w=majority'
+const dbURI = 'mongodb+srv://erickufta:LskwSZrEevOg2QDM@cluster0.a3a8ugn.mongodb.net/NomadTaxProMain?retryWrites=true&w=majority'
 mongoose.connect(dbURI)
-  .then((result) => app.listen(3001))
+  .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
   mongoose.set('debug', true)
@@ -28,44 +25,45 @@ app.use(morgan('dev'));
 app.use(express.static('public'));
 
 
-// app.get('/countries', (req, res) => {
-//   // const countries = JSON.parse(fs.readFileSync('./countries/countries.json', 'utf8'));
-//   const countries = Country.find({ display: true });
-//   console.log(countries);
-//   // const display_countries = underscore.where(countries, { 'display' : true })
-//   // res.render('countries', { 'countries' :  display_countries });
-// });
-
 app.get('/countries', (req, res) => {
-  Countries.find()
+  Countries.find( { Tagline : {$ne : null}} )
     .then(result => {
       console.log(result);
-      res.send(result);
+      res.render('countries', { 'countries' : result } );
+      next();
     })
     .catch(err => {
       console.log(err);
     });
 });
 
-app.get('/users', (req, res) => {
-  Customers.find()
-    .then(result => {
-      console.log(result);
-      res.send(result);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
 
 app.get('/', (req, res) => {
   res.redirect('/countries');
 });
 
-app.get('/signup', (req, res) => {
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup.ejs");
+});
+
+app.post('/signup', (req, res) => {
+  email = req.body.username;
+  password = req.body.password;
+
   res.render('signup');
 });
 
-app.get('tracker', (req, res) => {
+app.get('/login', (req, res) => {
+  email = req.body.username;
+  password = req.body.password;
+
+  res.render('login');
+});
+
+app.get('/tracker', (req, res) => {
   res.render('tracker');
 });
